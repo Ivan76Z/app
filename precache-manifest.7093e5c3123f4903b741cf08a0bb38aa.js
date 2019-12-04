2e676d9a371a246cd6309f62569303d4a7ac35d5
Ivan Zoyko <Ivan76Z@yandex.ru>
2019-12-04 19:54:02 +0300

    Updates

---
+++ precache-manifest.7093e5c3123f4903b741cf08a0bb38aa.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03fe12d759b77fc23df0b0c81287fc2c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f0dd3dd1e38b590431c",
+    "url": "/static/js/2.2442ae66.chunk.js"
+  },
+  {
+    "revision": "60dccb652a6b2671a935",
+    "url": "/static/js/main.a1bd17b3.chunk.js"
+  },
+  {
+    "revision": "7cfdc3162b515c309a68",
+    "url": "/static/js/runtime-main.f67fe4c9.js"
+  }
+]);
